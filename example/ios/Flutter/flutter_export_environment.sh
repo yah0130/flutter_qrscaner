ccbd22aaae168d983dabab076fc51061a9c6405e
yanhao <yanhao0130@hotmail.com>
2019-10-28 18:11:11 +0800

    修改版本

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/yanhao/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/yanhao/flutter_projects/flutter_qrscaner/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/yanhao/flutter/bin/cache/artifacts/engine/ios"
